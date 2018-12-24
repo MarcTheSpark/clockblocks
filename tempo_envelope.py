29790e76bf72b4d96af1a4f2bf3ce199f71fc34c
marc <marc.p.evans@gmail.com>
2018-12-23 23:24:15 -0800

    added support for TempoEnvelope class methods that take different unit types and also added the ability to specify units in show_plot

--- tempo_envelope.py
+++ tempo_envelope.py
@@ -3,10 +3,15 @@ from .expenvelope import Envelope, EnvelopeSegment
 
 class TempoEnvelope(Envelope):
 
-    def __init__(self, starting_rate=1.0):
+    def __init__(self, initial_rate_or_segments=1.0):
         # This is built on a envelope of beat length (units = s / beat, or really parent_beats / beat)
-        super().__init__()
-        self.initialize(1/starting_rate)
+        if isinstance(initial_rate_or_segments, list):
+            # this allows for compatibility with the Envelope constructor, helping class methods to work appropriately
+            assert all(isinstance(x, EnvelopeSegment) for x in initial_rate_or_segments)
+            super().__init__(initial_rate_or_segments)
+        else:
+            super().__init__()
+            self.initialize(1 / initial_rate_or_segments)
         self._t = 0.0
         self._beats = 0.0
 
@@ -117,6 +122,107 @@ class TempoEnvelope(Envelope):
         self._beats = b
         self._t = self.integrate_interval(0, b)
         return self
+
+    @staticmethod
+    def _convert_units(values, input_units, output_units):
+        in_units = input_units.lower().replace(" ", "")
+        out_units = output_units.lower().replace(" ", "")
+        assert in_units in ("tempo", "rate", "beatlength") and out_units in ("tempo", "rate", "beatlength"), \
+            "Invalid value of {} for units. Must be \"tempo\", \"rate\" or \"beat length\"".format(input_units)
+        if in_units == out_units:
+            return values
+        else:
+            convert_input_to_beat_length = (lambda x: 1 / x) if in_units == "rate" \
+                else (lambda x: 60 / x) if in_units == "tempo" else (lambda x: x)
+            convert_beat_length_to_output = (lambda x: 1 / x) if out_units == "rate" \
+                else (lambda x: 60 / x) if out_units == "tempo" else (lambda x: x)
+            if hasattr(values, "__len__"):
+                return tuple(convert_beat_length_to_output(convert_input_to_beat_length(x)) for x in values)
+            else:
+                return convert_beat_length_to_output(convert_input_to_beat_length(values))
+
+    @classmethod
+    def from_levels_and_durations(cls, levels=(0, 0), durations=(0,), curve_shapes=None, offset=0, units="tempo"):
+        super().from_levels_and_durations(TempoEnvelope._convert_units(levels, units, "beatlength"),
+                                          durations, curve_shapes, offset)
+
+    @classmethod
+    def from_levels(cls, levels, length=1.0, offset=0, units="tempo"):
+        super().from_levels(TempoEnvelope._convert_units(levels, units, "beatlength"), length=length, offset=offset)
+
+    @classmethod
+    def from_list(cls, constructor_list, units="tempo"):
+        assert hasattr(constructor_list, "__len__")
+        if hasattr(constructor_list[0], "__len__"):
+            # we were given levels and durations, and possibly curvature values
+            if len(constructor_list) == 2:
+                if hasattr(constructor_list[1], "__len__"):
+                    # given levels and durations
+                    return cls.from_levels_and_durations(constructor_list[0], constructor_list[1], units=units)
+                else:
+                    # given levels and the total length
+                    return cls.from_levels(constructor_list[0], length=constructor_list[1], units=units)
+
+            elif len(constructor_list) >= 3:
+                # given levels, durations, and curvature values
+                return cls.from_levels_and_durations(constructor_list[0], constructor_list[1], constructor_list[2],
+                                                     units=units)
+        else:
+            # just given levels
+            return cls.from_levels(constructor_list, units=units)
+
+    @classmethod
+    def from_points(cls, *points, units="tempo"):
+        assert all(len(point) >= 2 for point in points)
+        return super().from_points([(t, TempoEnvelope._convert_units(l, units, "beatlength"))
+                                    for (t, l) in points])
+
+    @classmethod
+    def release(cls, duration, start_level=1, curve_shape=None, units="tempo"):
+        curve_shapes = (curve_shape,) if curve_shape is not None else None
+        return cls.from_levels_and_durations((start_level, 0), (duration,), curve_shapes=curve_shapes, units=units)
+
+    @classmethod
+    def ar(cls, attack_length, release_length, peak_level=1, attack_shape=None, release_shape=None, units="tempo"):
+        return super().ar(attack_length, release_length, TempoEnvelope._convert_units(peak_level, units, "beatlength"),
+                          attack_shape=attack_shape, release_shape=release_shape)
+
+    @classmethod
+    def asr(cls, attack_length, sustain_level, sustain_length, release_length, attack_shape=None, release_shape=None,
+            units="tempo"):
+        return super().asr(attack_length, TempoEnvelope._convert_units(sustain_level, units, "beatlength"),
+                           sustain_length, release_length, attack_shape=attack_shape, release_shape=release_shape)
+
+    @classmethod
+    def adsr(cls, attack_length, attack_level, decay_length, sustain_level, sustain_length, release_length,
+             attack_shape=None, decay_shape=None, release_shape=None, units="tempo"):
+        return super().adsr(attack_length, TempoEnvelope._convert_units(attack_level, units, "beatlength"),
+                            decay_length, TempoEnvelope._convert_units(sustain_level, units, "beatlength"),
+                            sustain_length, release_length, attack_shape=attack_shape, decay_shape=decay_shape,
+                            release_shape=release_shape)
+
+    @classmethod
+    def from_function(cls, function, domain_start=0, domain_end=1, resolution_multiple=2,
+                      key_point_precision=100, key_point_iterations=5, units="tempo"):
+        converted_function = (lambda x: TempoEnvelope._convert_units(function(x), units, "beatlength")) \
+            if units.lower().replace(" ", "") != "beatlength" else function
+        return super().from_function(converted_function, domain_start, domain_end, resolution_multiple,
+                                     key_point_precision, key_point_iterations)
+
+    def show_plot(self, title=None, resolution=25, show_segment_divisions=True, units="tempo"):
+        try:
+            import matplotlib.pyplot as plt
+        except ImportError:
+            raise ImportError("Could not find matplotlib, which is needed for plotting.")
+        fig, ax = plt.subplots()
+        x_values, y_values = self.get_graphable_point_pairs(resolution)
+        ax.plot(x_values, TempoEnvelope._convert_units(y_values, "beat length", units))
+        if show_segment_divisions:
+            ax.plot(self.times, TempoEnvelope._convert_units(self.levels, "beat length", units), 'o')
+        plt.xlabel("Beat")
+        plt.ylabel("Tempo" if units == "tempo" else "Rate" if units == "rate" else "Beat Length")
+        ax.set_title('Graph of TempoEnvelope' if title is None else title)
+        plt.show()
 
     def __repr__(self):
         return "TempoEnvelope({}, {}, {})".format(self.levels, self.durations, self.curve_shapes)
