1063b56bf78b712454a1f7f8a561252a5b376d29
marc <marc.p.evans@gmail.com>
2019-01-11 21:01:46 -0800

    Re-organized to remove nested structure, instead relying on pypi for dependency on expenvelope. got setuptools working

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="clockblocks",
+    version="0.1.0",
+    author="Marc Evanstein",
+    author_email="marc@marcevanstein.com",
+    description="A python library for controlling the flow of musical time.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MarcTheSpark/clockblocks",
+    packages=setuptools.find_packages(),
+    install_requires=['expenvelope'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
