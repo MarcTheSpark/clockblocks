54f2163cf504a81fa3643d14fa007f8c389afd79
Marc Evanstein <marc@marcevanstein.com>
2020-08-06 17:45:41 -0700

    Added copyright notices to all files

--- clockblocks/clock.py
+++ clockblocks/clock.py
@@ -3,6 +3,22 @@ Module defining the central :class:`Clock` class, as well as the :class:`TimeSta
 beat in every clock at a given moment.
 """
 
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  #
+#  SCAMP (Suite for Computer-Assisted Music in Python)                                           #
+#  Copyright © 2020 Marc Evanstein <marc@marcevanstein.com>.                                     #
+#                                                                                                #
+#  This program is free software: you can redistribute it and/or modify it under the terms of    #
+#  the GNU General Public License as published by the Free Software Foundation, either version   #
+#  3 of the License, or (at your option) any later version.                                      #
+#                                                                                                #
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;     #
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     #
+#  See the GNU General Public License for more details.                                          #
+#                                                                                                #
+#  You should have received a copy of the GNU General Public License along with this program.    #
+#  If not, see <http://www.gnu.org/licenses/>.                                                   #
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  #
+
 from .tempo_envelope import MetricPhaseTarget, TempoEnvelope
 from expenvelope._utilities import _get_extrema_and_inflection_points
 from collections import namedtuple
