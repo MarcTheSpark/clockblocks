282f54a130483ce25fbc57919847d5d5316a4571
Marc Evanstein <marc@marcevanstein.com>
2026-05-12 22:06:46 -0700

    Cleaned up junk directory and stale stuff from utilities.py

--- cb2/utilities.py
+++ cb2/utilities.py
@@ -2,42 +2,9 @@ from __future__ import annotations
 import threading
 import time
 from threading import Event
-import cb2
-
-
-def sleep_precisely_until(stop_time: float, interruption_event: Event = None) -> None:
-    """
-    High-precision sleep until stop_time. Sleeps repeatedly by half of the remaining time until there are fewer than
-    500 microseconds left, at which point it implements a busy wait.
-
-    :param stop_time: desired stop time in seconds since the epoch (as is returned by :func:`time.time`)
-    :param interruption_event: (optional) an Event used to execute the sleep call. This has the advantage that the
-        sleep can be interrupted by calling `set()` on the event.
-    """
-    time_remaining = stop_time - time.time()
-    if time_remaining <= 0:
-        return
-    elif time_remaining < 0.0005:
-        # when there's fewer than 500 microseconds left, just burn cpu cycles and hit it exactly
-        while time.time() < stop_time and (interruption_event is None or not interruption_event.is_set()):
-            pass
-    else:
-        if interruption_event is not None:
-            if interruption_event.wait(timeout=time_remaining / 2):
-                return
-        else:
-            time.sleep(time_remaining / 2)
-        sleep_precisely_until(stop_time, interruption_event)
-
-
-def sleep_precisely(secs: float, interruption_event: Event = None) -> None:
-    """
-    High-precision sleep for the given number of seconds.
-
-    :param secs: sleep duration
-    :param interruption_event: see :func:`sleep_precisely_until`
-    """
-    sleep_precisely_until(time.time() + secs, interruption_event)
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    import clock
 
 
 def snap_float_to_nice_decimal(x: float, order_of_magnitude_difference=7) -> float:
@@ -70,15 +37,14 @@ class _PrintColors:
     END = '\033[0m'
 
 
-def current_clock() -> cb2.clock.Clock | None:
+def current_clock() -> clock.Clock | None:
     # utility for getting the clock we are currently using (we attach it to the thread when it's started)
     current_thread = threading.current_thread()
     return threading.current_thread().__clock__ if hasattr(current_thread, '__clock__') else None
 
 
 def wait(dt: float, units="beats") -> None:
-    c = current_clock()
-    if c is not None:
-        current_clock().wait(dt, units=units)
+    if (c := current_clock()) is not None:
+        c.wait(dt, units=units)
     else:
         time.sleep(dt)
